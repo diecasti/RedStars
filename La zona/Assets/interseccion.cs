8afe1396538790b41aa366caadf510f3e89a8cff
diecasti <62880649+diecasti@users.noreply.github.com>
2024-03-14 21:51:59 +0100

    interesecciones visuales

--- La zona/Assets/interseccion.cs
+++ La zona/Assets/interseccion.cs
@@ -25,14 +25,16 @@ public class interseccion : MonoBehaviour
 
     public void putLastDerecha(Transform child)
     {
+        Debug.Log("derecha");
+
         child.rotation = LastDerecha.rotation; //esto es por si, resulta que la ultima tile era la de una interseccion, porque aun ni idea de como voy a hacer para instanciarlas
         if (LastDerecha.GetComponent<MeshFilter>())
         {
-            child.position = LastDerecha.position + (LastDerecha.forward * ((child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z) + (LastDerecha.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * LastDerecha.localScale.z)));
+            child.position = LastDerecha.position - (LastDerecha.forward * ((child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z) + (LastDerecha.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * LastDerecha.localScale.z)));
         }
         else
         {
-            child.position = LastDerecha.position + (LastDerecha.forward * (child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z));
+            child.position = LastDerecha.position - (LastDerecha.forward * (child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z));
         }
         LastDerecha = child;
         derCout++;
@@ -41,13 +43,14 @@ public class interseccion : MonoBehaviour
     {
         child.rotation = LastIz.rotation; //esto es por si, resulta que la ultima tile era la de una interseccion, porque aun ni idea de como voy a hacer para instanciarlas
 
+            Debug.Log("izquierda");
         if (LastIz.GetComponent<MeshFilter>())
         {
-            child.position = LastIz.position + (LastIz.forward * ((child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z) + (LastIz.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * LastIz.localScale.z)));
+            child.position = LastIz.position - (LastIz.forward * ((child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z) + (LastIz.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * LastIz.localScale.z)));
         }
         else
         {
-            child.position = LastIz.position + (LastIz.forward * (child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z));
+            child.position = LastIz.position - (LastIz.forward * (child.GetComponent<MeshFilter>().sharedMesh.bounds.max.z * child.localScale.z));
         }
         LastIz = child;
         izCOunt++;
