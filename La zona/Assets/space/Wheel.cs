a35f4fe139af39e6b00a98f7ba042b77ccabcfa1
diecasti <62880649+diecasti@users.noreply.github.com>
2024-03-15 21:06:59 +0100

    minijuego de direccion terminado

--- La zona/Assets/space/Wheel.cs
+++ La zona/Assets/space/Wheel.cs
@@ -6,7 +6,8 @@ public class Wheel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPoi
 {
 
     bool selected;
-
+    Vector2 lastMouse;
+    public int valor;
     public void OnPointerClick(PointerEventData eventData)
     {
     }
@@ -21,6 +22,7 @@ public class Wheel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPoi
     public void OnPointerDown(PointerEventData eventData)
     {
         selected = true;
+        lastMouse = eventData.position;
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -28,12 +30,70 @@ public class Wheel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPoi
         selected = false;
     }
 
+
+    bool IsLeft(Vector2 A, Vector2 B) { return (-A.x * B.y + A.y * B.x < 0); }
+    float HowManyLeft(Vector2 A, Vector2 B) { return (-A.x * B.y + A.y * B.x); }
+
+
     private void Update()
     {
         
         if (selected)
         {
+
+            //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector3 direction = mousePosition - transform.position;
+            //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            //transform.localRotation *= Quaternion.AngleAxis(angle, Vector3.up);
+
+
             //modificar la rotacion de Z dependiendo de la posicion del raton, la vamos a ir sumando anda mas la diferencia
+            Vector2 mousePos = Input.mousePosition;
+           
+            Vector2 centro = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+            //calculando la diferencia entre el punto anterior y el actual, ver hacia que lugar hay que darle inercia
+            Vector2 delta = (mousePos - centro);
+            Vector2 ori = (lastMouse - centro);
+
+            Vector3 cross = Vector3.Cross(delta, ori);
+
+
+
+
+            if (cross == Vector3.zero)
+            {
+                // Target is straight ahead
+            }
+            else if (!IsLeft(delta, ori))
+            {
+
+
+                float multiplyFactor = Mathf.Abs(HowManyLeft(delta, ori));
+                multiplyFactor = multiplyFactor / 100.0f;
+                if (multiplyFactor > 10)
+                    multiplyFactor = 3;
+
+                // Target is to the right
+                //Debug.Log(cross.y);
+
+                transform.localRotation *= Quaternion.AngleAxis(-1 * multiplyFactor, Vector3.up);
+                valor += (-1 * (int)multiplyFactor);
+            }
+            else
+            {
+
+                float multiplyFactor = Mathf.Abs(HowManyLeft(delta, ori));
+                multiplyFactor = multiplyFactor / 100.0f;
+                if (multiplyFactor > 10)
+                    multiplyFactor = 3;
+                //Debug.Log("izquierda");
+                // Target is to the left
+                transform.localRotation *= Quaternion.AngleAxis(1 * multiplyFactor, Vector3.up);
+                valor += (1 * (int)multiplyFactor);
+            }
+
+            lastMouse = mousePos;
         }
     }
 
--- La zona/Assets/space/minijuego_direccion.cs
+++ La zona/Assets/space/minijuego_direccion.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class minijuego_direccion : minijuego
@@ -7,11 +8,25 @@ public class minijuego_direccion : minijuego
 
     public Vector3 direccion;
 
-    public Transform volanteX;
-    public Transform volanteY;
-    public Transform volanteZ;
+    public Wheel volanteX;
+    public Wheel volanteY;
+    public Wheel volanteZ;
 
     public float sensitivity = 5;
+
+
+    public int desiredX = 1000;
+    public int desiredY = 2000;
+    public int desiredZ = 5000;
+
+    public bool goodX = false;
+    public bool goodY = false;
+    public bool goodZ = false;
+
+
+    public TextMeshPro textoX;
+    public TextMeshPro textoY;
+    public TextMeshPro textoZ;
 
 
     // Update is called once per frame
@@ -22,19 +37,42 @@ public class minijuego_direccion : minijuego
             //habilitar uin cursor, o que se haga mediante teclas? mejor un cursor en mi opinion
             //pero mientras tanto vamos a ponerlo con teclas
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                volanteX.localRotation *= Quaternion.AngleAxis(sensitivity * 1, Vector3.up);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                volanteY.localRotation *= Quaternion.AngleAxis(sensitivity * 1, Vector3.up);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                volanteZ.localRotation *= Quaternion.AngleAxis(sensitivity * 1, Vector3.up);
-            }
+            //if (Input.GetKey(KeyCode.A))
+            //{
+            //    volanteX.localRotation *= Quaternion.AngleAxis(sensitivity * 1, Vector3.up);
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    volanteY.localRotation *= Quaternion.AngleAxis(sensitivity * 1, Vector3.up);
+            //}
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    volanteZ.localRotation *= Quaternion.AngleAxis(sensitivity * 1, Vector3.up);
+            //}
+
+
 
         }
+
+        
+        textoX.text = "X desired " + desiredX + "\nX Angle: " + volanteX.valor;
+        textoY.text = "Y desired " + desiredY + "\nY Angle: " + volanteY.valor;
+        textoZ.text = "Z desired " + desiredZ + "\nZ Angle: " + volanteZ.valor;
+        //Debug.Log("direction " + checkDirection());
+
+
     }
+
+
+    public bool checkDirection()
+    {
+
+            goodX = Mathf.Abs(desiredX - volanteX.valor) <= 10;
+            goodY = Mathf.Abs(desiredY - volanteY.valor) <= 10;
+            goodZ = Mathf.Abs(desiredZ - volanteZ.valor) <= 10;
+
+        return (goodX && goodY && goodZ);
+    }
+
+
 }
